9c490d1e722d454290cd23b091fdd15f52ea0586
user112489 <Ali@mohammads-macbook-pro.local>
2016-10-26 21:51:52 +0200

    Initial Commit

---
+++ Weather Animation App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Animation App
+//
+//  Created by Ali Akkawi on 26/10/2016.
+//  Copyright © 2016 Ali Akkawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
