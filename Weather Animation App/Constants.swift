d0c73cd98afe2e5693e372b2a67935b5164d914c
Ali Akkawi <Ali@Mohammads-MacBook-Pro.local>
2016-10-29 16:54:27 +0200

    first commit

---
+++ Weather Animation App/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Weather Animation App
+//
+//  Created by Ali Akkawi on 26/10/2016.
+//  Copyright © 2016 Ali Akkawi. All rights reserved.
+//
+
+import Foundation
---
+++ Weather Animation App/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Weather Animation App
+//
+//  Created by Ali Akkawi on 28/10/2016.
+//  Copyright © 2016 Ali Akkawi. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
